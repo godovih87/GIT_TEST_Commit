6366851c80aee91a6ad38a79232156b08849bb9b
godovih87 <egorgodovihggplay@mail.ru>
2025-03-13 11:07:38 +0500

    first commit

---
+++ Test_TRPO_Commit/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_TRPO_Commit
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int a = rnd.Next();
+            label1.Text = a.ToString();
+        }
+    }
+}
